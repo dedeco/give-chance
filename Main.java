5e90bb4c660e4cb57bf36df57a06d672ba947667
Dedeco <dedeco@gmail.com>
2022-06-07 03:12:43 +0000

    Now, works!

--- Main.java
+++ Main.java
@@ -1,21 +1,50 @@
-
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.Arrays;
 
 class Main {
   // write your solution in this function
   public static double GiveChange(CatalogItem[] catalogItems, OrderedItem[] orderedItems, double moneyProvided) throws IllegalArgumentException, ItemNotInCatalogException, InsufficientFundsException {
 
-  HashMap<Integer, Double> map = new HashMap<>();
+  if (moneyProvided < 0)
+    throw new IllegalArgumentException();
+
+  if ( orderedItems == null 
+      || (catalogItems == null && orderedItems == null)
+     )
+    return moneyProvided;
     
+  if (catalogItems == null)
+    throw new ItemNotInCatalogException();
+
+  Map<Integer, Double> map = new HashMap<>();
+ 
   for(OrderedItem item: orderedItems){
-    if (!map.containsKey(item.id)){
-      price = catalogItems.stream()
-        .filter(catalog -> (catalog.getId() == item.getId()))
+    if (item == null)
+      break;
+    
+    if (!map.containsKey(item.getId())){
+      CatalogItem cat = Arrays
+        .stream(catalogItems)
+        .filter(catalogItem -> catalogItem.getId() == item.getId())
         .findAny()
-        .orElse(0.0);
-      map.put(item.id, price * item.quantity);
+        .orElse(null);
+      
+      if (cat == null)
+        throw new ItemNotInCatalogException();
+      
+      map.put(item.getId(), cat.getPrice() * item.getQuantity());
     }  
   }
-  return map.values().stream().mapToDouble(Double::parseDouble).sum();;
+    
+  double charge = map.values().stream().reduce(0.0, Double::sum);
+  double chance = moneyProvided - charge;
+
+  if (chance < 0)
+    throw new InsufficientFundsException();
+    
+  return chance;
 }
 
   // do not change the following code
